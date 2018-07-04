3dd422efd74203ba30f7a8a7e3450f0b96682ede
Alex Koval <chuguyster@gmail.com>
2018-07-04 15:05:58 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,5 +1,7 @@
 /* <sl:translate> */
 //Cookies
+var a = 'New JS Text to ingest';
+var b = 'No wrapped HTML';
 var c = 'Text %s translation ABCDEFG';
 var d = 'Already %s masked text';
 /* </sl:translate> */
