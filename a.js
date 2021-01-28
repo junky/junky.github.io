062373a40befbbfb87d3e282ff13d24c77c5715a
Alex Koval <akoval@smartling.com>
2021-01-28 17:38:48 +0200

    Update a.js

--- a.js
+++ a.js
@@ -4,4 +4,5 @@ var a = 'New JS Text to ingest';
 var b = 'No wrapped HTML';
 var c = 'Text %s translation ABCDEFG';
 var d = 'Already %s masked text';
+var e = 'Enter JS text "Transaction # ".';
 /* </sl:translate> */
