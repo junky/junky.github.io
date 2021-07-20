c627dd003814e3457f0af9fceaa63f9f320343b9
Alex Koval <akoval@smartling.com>
2021-07-20 11:28:32 +0300

    Update a.js

--- a.js
+++ a.js
@@ -9,3 +9,4 @@ var f = 'String with \n new line';
 /* </sl:translate> */
 
 var md = /*<sl:translate_md> <first-string/>*/'**String with** \n\n # new line';
+var pt = /*<sl:translate> <first-string/>*/"Translatable text";
